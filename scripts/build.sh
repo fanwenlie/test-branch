f9f1efb84ba328afe4aa77597a9f7b6825f50a1a
fanwenlie <fanwenlie@yy.com>
2021-08-10 17:22:47 +0800

    🐳 chore: 增加git命令脚本

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+set -e
+
+VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]'`
+
+echo "Releasing $VERSION ..."
+
+git add -A
+git commit -m "[build] $VERSION"
+npm version $VERSION --message "[release] $VERSION"
+
+
+# publish
+git push origin main
+git push origin refs/tags/v$VERSION
+git checkout dev
+git rebase main
+git push origin dev
