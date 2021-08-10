7d206f13664f2d5f8faa64c3155de66047684eae
fanwenlie <fanwenlie@yy.com>
2021-08-10 17:36:42 +0800

    [build] 1.0.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,12 @@ echo "Releasing $VERSION ..."
 
 git add -A
 git commit -m "[build] $VERSION"
+git tag v$VERSION
 # npm version $VERSION --message "[release] $VERSION"
 
 # publish
 git push origin main
-git push origin refs/tags/v$VERSION
+git push origin v$VERSION
 git checkout dev
 git rebase main
 git push origin dev
