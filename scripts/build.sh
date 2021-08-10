596c3416e75385d2e29aa4120869ad82c01a5e43
fanwenlie <fanwenlie@yy.com>
2021-08-10 18:14:53 +0800

    🐎 ci: 只push tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,17 @@
 set -e
 
 VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]'`
-
+TAG_VERSION = v$VERSION
 echo "Releasing $VERSION ..."
 
-git add -A
-git commit -m "[build] $VERSION"
-git tag v$VERSION
+# git add -A
+# git commit -m "[build] $VERSION"
 # npm version $VERSION --message "[release] $VERSION"
+git tag $TAG_VERSION
 
 # publish
-git push origin main
-git push origin v$VERSION
+# git push origin main
+git push origin $TAG_VERSION
 git checkout dev
 git rebase main
 git push origin dev
