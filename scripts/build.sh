20896c4ca1c1fd5db8de80dff472dca05d5c56cc
fanwenlie <fanwenlie@yy.com>
2021-08-11 15:37:22 +0800

    修改sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+git checkout main
+
 VERSION=`cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]'`
 
 TAG_VERSION=v$VERSION
